b84afe61bfcc6875546e0fc8bbdb72b7374b3736
Ronald Head <ronaldlhead@gmail.com>
2022-01-13 09:29:06 -0700

    Test click functionality on first box

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ var p2Wins = [];
 var whoseTurn = document.getElementById("whoseTurn");
 var p1Score = document.getElementById("pl1Score");
 var p2Score = document.getElementById("p2Score")
+var box1 = document.getElementById("box1");
+
 //event listeners go here
+box1.addEventListener("click", addToken);
 
 //functions go here
+function addToken() {
+  var playerOne = new Player(event.target.id, "X", 0);
+  //add if statement to see whose turn
+  box1.innerHTML += `
+  <article class="game-square" id="box1">${playerOne.token}</article`;
+}
