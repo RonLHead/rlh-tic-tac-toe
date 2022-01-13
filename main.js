9fdcf5ec56b9d821707b776133d52d732fc07061
Ronald Head <ronaldlhead@gmail.com>
2022-01-13 10:01:45 -0700

    Test array functionality of gamegrid

--- main.js
+++ main.js
@@ -5,15 +5,26 @@ var p2Wins = [];
 var whoseTurn = document.getElementById("whoseTurn");
 var p1Score = document.getElementById("pl1Score");
 var p2Score = document.getElementById("p2Score")
-var box1 = document.getElementById("box1");
+// var box1 = document.getElementById("box1");
+var boxes = document.querySelectorAll(".game-square")
 
 //event listeners go here
-box1.addEventListener("click", addToken);
+// box1.addEventListener("click", addToken);
 
 //functions go here
-function addToken() {
-  var playerOne = new Player(event.target.id, "X", 0);
-  //add if statement to see whose turn
-  box1.innerHTML += `
-  <article class="game-square" id="box1">${playerOne.token}</article`;
+// function addToken() {
+//   var playerOne = new Player(event.target.id, "X", 0);
+//   //add if statement to see whose turn
+//   box1.innerHTML += `
+//   <article class="game-square" id="box1">${playerOne.token}</article`;
+// }
+
+var handleClick = (e) => {
+  var location = e.target.id[1];
+
+  console.log("location", location)
+}
+
+for(var i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener('click', handleClick)
 }
