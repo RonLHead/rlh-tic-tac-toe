0001b1d0421093c962d4d3886ac089f846081b4b
Ronald Head <ronaldlhead@gmail.com>
2022-01-13 08:36:58 -0700

    Add main.js

--- game.js
+++ game.js
@@ -2,8 +2,8 @@ class Game {
   constructor(player1, player2) {
     this.player1 = player1;
     this.player2 = player2;
-    this.player1Score = 0;
-    this.player2Score = 0;
+    this.player1Score = player1.wins;
+    this.player2Score = player2.wins;
     this.player1Turn = true;
     this.player1Turn = false;
     this.draw = false;
@@ -22,7 +22,7 @@ class Game {
   }
 
   resetGame() {
-    
+
   }
 
 }
---
+++ main.js
@@ -0,0 +1,8 @@
+var p1Wins = [];
+var p2Wins = [];
+
+//query selectors go here
+
+//event listeners go here
+
+//functions go here
