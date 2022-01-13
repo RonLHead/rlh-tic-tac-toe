de1f3e959c4aa21918213a7982992447608532f7
Ronald Head <ronaldlhead@gmail.com>
2022-01-13 13:44:37 -0700

    Add clear grid function and refactor

--- main.js
+++ main.js
@@ -11,10 +11,10 @@ var box1 = document.getElementById("box1");
 var boxes = document.querySelectorAll(".game-square");
 
 for (var i = 0; i < boxes.length; i++) {
-  boxes[i].addEventListener("click", playerClick);
+  boxes[i].addEventListener("click", addToken);
 }
 
-function playerClick(e) {
+function addToken(e) {
   var location = e.target;
 
   if (location.innerText === "" || location.innerText === "") {
@@ -23,15 +23,34 @@ function playerClick(e) {
     <article class="game-square player-one-token"
     id="${location.id}">
     ${player1.token}</article>`;
-      whoseTurn.innerText = "It's Player Two's Turn!";
-      game.nextPlayer(player1, player2);
-    } else if (game.player2Turn) {
+      whichPlayersTurn();
+    } else {
       location.innerHTML += `
     <article class="game-square player-two-token"
     id="${location.id}">
     ${player2.token}</article>`;
-      whoseTurn.innerText = "It's Player One's Turn!";
-      game.nextPlayer(player1, player2);
+      whichPlayersTurn();
     }
+  }
+}
+
+function whichPlayersTurn() {
+  game.nextPlayer(player1, player2);
+  if(player2.id) {
+    whoseTurn.innerText = "It's Player Two's Turn!";
+  } else {
+    whoseTurn.innerText = "It's Player One's Turn!"
+  }
+}
+
+function clearGameGrid() {
+  for(var i=0; i < boxes.length; i++) {
+    boxes[i].innerText = "";
+  }
+}
+
+function tieGame() {
+  if(boxes.innerText === "X" || boxes.innerText === "O") {
+    clearGameGrid();
   }
 }
