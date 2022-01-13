1a28b8fed36cf84fce8cb710e9b2e45cc7303668
Ronald Head <ronaldlhead@gmail.com>
2022-01-13 15:16:33 -0700

    Work on 3 in a row functionality

--- main.js
+++ main.js
@@ -3,28 +3,40 @@ var player2 = new Player(false, "O", 0);
 var game = new Game(player1, player2);
 var p1Wins = [];
 var p2Wins = [];
-var areBoxesFilled = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
 
 var whoseTurn = document.getElementById("whoseTurn");
 var p1Score = document.getElementById("pl1Score");
 var p2Score = document.getElementById("p2Score");
-var box1 = document.getElementById("box1");
-var box2 = document.getElementById("box2");
-var box3 = document.getElementById("box3");
-var box4 = document.getElementById("box4");
-var box5 = document.getElementById("box5");
-var box6 = document.getElementById("box9");
-var box7 = document.getElementById("box7");
-var box8 = document.getElementById("box8");
-var box9 = document.getElementById("box0");
+var filledbox1 = document.getElementById("box1");
+// var box2 = document.getElementById("box2");
+// var box3 = document.getElementById("box3");
+// var box4 = document.getElementById("box4");
+// var box5 = document.getElementById("box5");
+// var box6 = document.getElementById("box9");
+// var box7 = document.getElementById("box7");
+// var box8 = document.getElementById("box8");
+// var box9 = document.getElementById("box9");
 var boxes = document.querySelectorAll(".game-square");
 
+var box1 = boxes[0].innerText;
+var box2 = boxes[1].innerText;
+var box3 = boxes[2].innerText;
+var box4 = boxes[3].innerText;
+var box5 = boxes[4].innerText;
+var box6 = boxes[5].innerText;
+var box7 = boxes[6].innerText;
+var box8 = boxes[7].innerText;
+var box9 = boxes[8].innerText;
+
+var areBoxesFilled = [box1, box2, box3]
 for (var i = 0; i < boxes.length; i++) {
   boxes[i].addEventListener("click", addToken);
 }
-whoseTurn.addEventListener("click", clearGameGrid);
+// whoseTurn.addEventListener("click", clearGameGrid);
+
 
 function addToken(e) {
+  e.preventDefault()
   var location = e.target;
 
   if (location.innerText === "" || location.innerText === "") {
@@ -34,6 +46,7 @@ function addToken(e) {
     id="${location.id}">
     ${player1.token}</article>`;
       whichPlayersTurn();
+
     } else {
       location.innerHTML += `
     <article class="game-square player-two-token"
@@ -42,6 +55,7 @@ function addToken(e) {
       whichPlayersTurn();
     }
   }
+  threeInARow()
   tieGame();
 }
 
@@ -55,13 +69,22 @@ function whichPlayersTurn() {
 }
 
 function clearGameGrid() {
-  for(var i=0; i < boxes.length; i++) {
-    boxes[i].innerText = "";
+  if(game.draw) {
+    for(var i=0; i < boxes.length; i++) {
+      boxes[i].innerText = "";
+    }
   }
+  resetToPlayerOne();
+  game.draw = false;
 }
 
 function resetToPlayerOne() {
   whoseTurn.innerText = "It's Player One's Turn!";
+  game.player1Turn = true;
+}
+
+function tieDisplay() {
+  whoseTurn.innerText = "It's a Tie!"
 }
 
 function tieGame() {
@@ -73,15 +96,20 @@ function tieGame() {
   }
 
   if(allFilled === 9) {
-    // whoseTurn.innerText = "It's a Tie!"
-    whoseTurn.innerHTML = `
-    `
-    // clearGameGrid();
-    // resetToPlayerOne()
+    tieDisplay();
+    game.draw = true;
+    setTimeout("clearGameGrid()", 3000)
   }
-  //check if game is tied
 }
 
 function threeInARow() {
-
+  for(var i = 0; i < boxes.length; i++) {
+    areBoxesFilled[i] = boxes[i].innerText;
+  }
+  if(areBoxesFilled[0] === areBoxesFilled[1] === areBoxesFilled[2]) {
+    player1.wins += 1;
+    whoseTurn.innerText = "Player One Wins!"
+    ps1Score.innerText = `${player1.wins}`;
+    return true;
+  }
 }
